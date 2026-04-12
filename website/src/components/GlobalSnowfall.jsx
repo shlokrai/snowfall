1d9a2cab70992138467b8e356e403e9c811d75fc
shlokrai <shlokrai@users.noreply.github.com>
2026-04-13 04:57:40 +0530

    Fix GlobalSnowfall.jsx UTF-16 encoding to UTF-8

--- website/src/components/GlobalSnowfall.jsx
+++ website/src/components/GlobalSnowfall.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 
 const GlobalSnowfall = () => {
   const [flakes, setFlakes] = useState([]);
