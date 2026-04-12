9c865351d0c6636bd2ed9adce708fbc57d7bb9af
shlokrai <shlokrai@users.noreply.github.com>
2026-04-13 05:00:41 +0530

    fix: re-import GlobalSnowfall component in App.jsx

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-d
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LoadingScreen from './components/LoadingScreen';
+import GlobalSnowfall from './components/GlobalSnowfall';
 import Home from './pages/Home';
 import GlobalHome from './pages/GlobalHome';
 import ContactUs from './pages/ContactUs';
@@ -36,6 +37,7 @@ function AppContent() {
 
   return (
     <div className={`flex flex-col min-h-screen font-sans bg-gray-50 text-gray-900 ${isGlobal ? 'global-theme' : ''}`}>
+      {isSnowingGlobally && <GlobalSnowfall />}
       {isLoading && <LoadingScreen transitionState={transitionState} />}
       <Header isSnowingGlobally={isSnowingGlobally} toggleSnowfall={() => setIsSnowingGlobally(!isSnowingGlobally)} />
       <main className="flex-grow">
